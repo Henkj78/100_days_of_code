cf618141c943d8155918be053c2ff9d96cb6084d
HenkJ78 <henkjvanhouten@gmail.com>
2022-11-11 22:22:07 +0100

    Challenge 2 of Hangman

--- day_7_Hangman.py
+++ day_7_Hangman.py
@@ -1,16 +1,29 @@
 import random
 
-word_list = ["ardvark", "baboo", "camel"]
+# Exercise 2
+#task 1:
+# Create an empty list called display. for each letter in the chosen_word, add a "_" to display.
 
-# chose a random word and assign it to chosen_word
+#task 2:
+# Loop through each position in the chosen_word:
+# if the letter at that position matches 'guess' then reveal that letter in the display at that position
+
+word_list = ["ardvark", "baboo", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-chosen_letter = input("Guess a letter: ").lower()
-
+display = []
 for letter in chosen_word:
-    if letter == chosen_letter:
-        print(True)
-    else:
-        print(False)
+    display.append("_")
+
+print(display)
+
+chosen_letter = input("\nGuess a letter: ").lower()
+
+for letter in chosen_letter:
+    if letter in chosen_word:
+        index_positions = chosen_word.index(letter)
+        display[index_positions] = letter
+
+print(display)
