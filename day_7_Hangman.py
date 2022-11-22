e57529853fccf3dadfb5b1dc39bb49fb8257fe2d
HenkJ78 <henkjvanhouten@gmail.com>
2022-11-22 08:46:27 +0100

    Challange 3 Hangman adjustments

--- day_7_Hangman.py
+++ day_7_Hangman.py
@@ -1,17 +1,69 @@
 import random
 
-# Exercise 2
-#task 1:
-# Create an empty list called display. for each letter in the chosen_word, add a "_" to display.
-
-#task 2:
-# Loop through each position in the chosen_word:
-# if the letter at that position matches 'guess' then reveal that letter in the display at that position
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["ardvark", "baboo", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
+# Player had 6 lives at the start of the game
+lives = 6
 
 display = []
 for letter in chosen_word:
@@ -29,8 +81,16 @@ while not end_of_game:
         if letter == chosen_letter:
             display[i] = chosen_letter
 
+    if chosen_letter not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+
     print(display)
 
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+    print(stages[lives])
